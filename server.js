015517e7c1f7b85a5c88cb0c3f442f7b33991292
kasaden <133546639+kasaden@users.noreply.github.com>
2026-08-27 17:09:51 +0200

    add link previews and credit the source spreadsheet

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ const MIME = {
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".css": "text/css; charset=utf-8",
-  ".json": "application/json; charset=utf-8"
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png"
 };
 
 function sendJson(response, status, payload) {
